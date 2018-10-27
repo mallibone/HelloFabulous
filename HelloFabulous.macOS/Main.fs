e76b652dee1f0d65843d188f3ee7ab822b84add6
Mark <mallibone@outlook.com>
2018-10-27 19:23:44 +0200

    Initial commit

---
+++ HelloFabulous.macOS/Main.fs
@@ -0,0 +1,11 @@
+﻿namespace HelloFabulous.macOS
+open System
+open AppKit
+
+module main =
+    [<EntryPoint>]
+    let main args =
+        NSApplication.Init ()
+        NSApplication.SharedApplication.Delegate <- new AppDelegate(); // add this line
+        NSApplication.Main (args)
+        0
